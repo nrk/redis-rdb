55b83b0731e7c59c70317d3c7732a63826228ff6
Daniele Alessandri <suppakilla@gmail.com>
2012-03-25 20:44:22 +0200

    Fix rdb path in example.

--- examples/read_rdb.rb
+++ examples/read_rdb.rb
@@ -2,4 +2,8 @@ $:.unshift File.expand_path('../lib', File.dirname(__FILE__))
 
 require 'rdb'
 
-RDB::Reader.read_file('test/rdb/multiple_databases.rdb', callbacks: RDB::DebugCallbacks.new)
+options = {
+  callbacks: RDB::DebugCallbacks.new,
+}
+
+RDB::Reader.read_file('test/rdb/database_multiple_logical_dbs.rdb', options)
