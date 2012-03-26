cfbe688c8f68176aa8de6a68fbb990dd9d01c598
Daniele Alessandri <suppakilla@gmail.com>
2012-03-26 23:46:56 +0200

    First stab at support for variadic commands in AOF.

--- examples/aof_dumper.rb
+++ examples/aof_dumper.rb
@@ -5,4 +5,4 @@ require 'rdb'
 source = 'test/rdb/database_multiple_logical_dbs.rdb'
 destination = File.basename(source, '.rdb') + '.aof'
 
-RDB::Dumpers::AOF.new(source, destination).dump
+RDB::Dumpers::AOF.new(source, destination, variadic: true).dump
